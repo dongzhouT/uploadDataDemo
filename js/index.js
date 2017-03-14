3c557411888f37fa288a6d2d0a52fef6fe74a9f2
zhou2020 <taodongzhou@163.com>
2017-03-14 16:01:08 +0800

    change ip

--- js/index.js
+++ js/index.js
@@ -29,6 +29,9 @@ $(function() {
 		selectData();
 	});
 	$("#submit").click(function() {
+		if (!confirm("确认要提交？")) {
+			return;
+		}
 		dataArrays = new Array();
 		$(".data-tr").each(function() {
 			//			var item = new $.dataPost();
@@ -37,7 +40,7 @@ $(function() {
 			//			item.active = $(this).find(".activeData").val();
 			//			dataArrays.push(item);
 			//另一种写法
-			var item2=new Object();
+			var item2 = new Object();
 			item2["time"] = $(this).find(".timeDate").attr("data-time");
 			item2["register"] = $(this).find(".registerData").val();
 			item2["active"] = $(this).find(".activeData").val();
@@ -50,7 +53,7 @@ $(function() {
 		m_days = JSON.stringify(request);
 		$.ajax({
 			type: "post",
-			url: "http://192.168.43.2:8080/writePlan",
+			url: "http://115.28.214.40:8080/WisdomCloudStore/writePlan",
 
 			async: true,
 			data: {
@@ -68,7 +71,7 @@ $(function() {
 				}
 			},
 			error: function(obj, msg, msg2) {
-				console.log("errorMsg:" + msg);
+				console.log("errorMsg:" + msg + "," + msg2);
 			},
 			beforeSend: function() {
 				$(".load-txt").text("正在提交...");
@@ -86,16 +89,27 @@ function getFormatDate(dateTime) {
 	return (dateTime.getMonth() + 1) + "/" + dateTime.getDate() + "/" + dateTime.getFullYear();
 }
 
+function getWeek(dateTime) {
+	var a = new Array("周日", "周一", "周二", "周三", "周四", "周五", "周六");
+	var index = dateTime.getDay();
+	return a[index];
+}
+
 function getFormatDate2(dateTime) {
 	return dateTime.getFullYear() + "-" + (dateTime.getMonth() + 1) + "-" + dateTime.getDate();
 }
 
+function getFormatDateHms(dateTime) {
+	return dateTime.getFullYear() + "-" + (dateTime.getMonth() + 1) + "-" + dateTime.getDate() + " " + dateTime.getHours() +
+		":" + dateTime.getMinutes() + ":" + dateTime.getSeconds();
+}
+
 function selectData() {
-	console.log("url:" + "http://192.168.43.2:8080/readPlan?start_time=" + startTime + "&end_time=" + endTime);
+	console.log("url:" + "http://115.28.214.40:8080/WisdomCloudStore/readPlan?start_time=" + startTime + "&end_time=" + endTime);
 	$.ajax({
 		type: "post",
 		//		url: "http://10.156.184.89/dataInput/index.php",
-		url: "http://192.168.43.2:8080/readPlan",
+		url: "http://115.28.214.40:8080/WisdomCloudStore/readPlan",
 		async: true,
 		data: {
 			"start_time": startTime,
@@ -123,18 +137,20 @@ function selectData() {
 
 }
 
-function getDataByTime(date) {
+function getDataByTime(date, times) {
 	var registerData = 0;
 	var activeData = 0;
+	var timeData = times;
 	for (var i = 0; i < m_obj.data.length; i++) {
 		var d = m_obj.data[i].time;
 		if (date == getFormatDate2(new Date(d * 1000))) {
 			registerData = m_obj.data[i].register;
 			activeData = m_obj.data[i].active;
+			timeData = m_obj.data[i].time;
 			break;
 		}
 	}
-	var temp = [registerData, activeData];
+	var temp = [registerData, activeData, timeData];
 	return temp;
 }
 
@@ -144,12 +160,14 @@ function addDays() {
 	var lines = "";
 	$("#data-table tr:gt(0)").remove();
 	for (var i = 0; i < m_days; i++) {
-		var times = startTime + i * 3600 * 24;
+		//十点~11点
+		var times = parseInt(startTime + i * 3600 * 24 + 3600 * 14 + 3600 * Math.random());
 		var tempDate = new Date(times * 1000);
 		var tdate = getFormatDate2(tempDate);
-		var info = getDataByTime(tdate);
+		var info = getDataByTime(tdate, times);
+		var ymdhmsDate = getFormatDateHms(new Date(info[2] * 1000)) + " " + getWeek(tempDate);
 		lines += "<tr class=\"data-tr\">\r\n" +
-			"				<td class=\"timeDate\" data-time=\"" + times + "\">" + tdate + "</td>\r\n" +
+			"				<td class=\"timeDate\" data-time=\"" + info[2] + "\">" + tdate + "</td>\r\n" +
 			"				<td class=\"registerNum\"><input type=\"text\" class=\"registerData\" value=\"" + info[0] + "\"></td>\r\n" +
 			"				<td class=\"activeNum\"><input type=\"text\" class=\"activeData\"  value=\"" + info[1] + "\"></td>\r\n" +
 			"			</tr>";
